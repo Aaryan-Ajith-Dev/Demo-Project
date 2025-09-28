4b28bd3cf5d5a6aa1bb4d46d691b1dc48fa40f25
Aaryan-Ajith-Dev <aaryan.dev@iiitb.ac.in>
2025-09-28 12:09:10 +0530

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-package java.org.example;
+package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
