4a89ce1cedc100d9ccd5e89503120e31abcf56da
Aaryan-Ajith-Dev <aaryan.dev@iiitb.ac.in>
2025-10-02 17:58:14 +0530

    fix

--- src/main/java/org/example/services/CalculatorService.java
+++ src/main/java/org/example/services/CalculatorService.java
@@ -4,7 +4,7 @@ public class CalculatorService {
     public double sqrt(double x) {
         return Math.sqrt(x);
     }
-    public long fact(long x) {
+    public long fact(int x) {
         assert x >= 0;
         if (x == 0) return 1;
         return x * fact(x - 1);
